27c6fef5bbc39ab3b734089dc0dd401708cd7cb5
Ruslan <ruslan.shtybaev@gmail.com>
2022-02-16 22:10:09 +0300

    Initial Commit

---
+++ rgbViewOnUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgbViewOnUIKit
+//
+//  Created by Руслан Штыбаев on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
