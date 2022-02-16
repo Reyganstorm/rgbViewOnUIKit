99e3f444d98d324320295de2450bd921ab9497c4
Ruslan <ruslan.shtybaev@gmail.com>
2022-02-16 23:27:09 +0300

    1. fix constraints in main.storyboard 2. setup logic rgb in ViewControler

--- rgbViewOnUIKit/ViewController.swift
+++ rgbViewOnUIKit/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 20
+        
+        redLabel.text =
+        String(format: "%.0f", redSlider.value)
+        rgbColorChanging()
+        greenLabel.text = String(format: "%.0f", greenSlider.value)
+        rgbColorChanging()
+        blueLabel.text = String(format: "%.0f", blueSlider.value)
+        rgbColorChanging()
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabel.text =
+        String(format: "%.0f", redSlider.value)
+        rgbColorChanging()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.0f", greenSlider.value)
+        rgbColorChanging()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.0f", blueSlider.value)
+        rgbColorChanging()
+    }
+    
+    
+    private func rgbColorChanging() {
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value/366), green: CGFloat(greenSlider.value/366), blue: CGFloat(blueSlider.value/366), alpha: 1)
+    }
 }
 
